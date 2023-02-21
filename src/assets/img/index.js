890c0612741d1ec1cd09e41675f35f2a56636c63
Rory Deken <rorydeken@gmail.com>
2023-02-21 13:02:09 -0500

    updates

--- src/assets/img/index.js
+++ src/assets/img/index.js
@@ -1,4 +1,5 @@
-export const Chair = require('./chair.png'); 
+export const Chair = require('./chair.png');
+export const Logo = require('./logo-white.png');
 export const Michelle = require('./michelle.png');
 export const Location = require('./location.png');
 export const Job = require('./job.png');
--- src/components/menu.js
+++ src/components/menu.js
@@ -1,37 +1,23 @@
-import React, { useState } from 'react'
-import {ReactComponent as Logo} from '../assets/img/logo.svg';
+import {Logo} from '../assets/img';
 export default function Menu() {
-    const [toggle, setToggle] = useState(true)
     return (
     <div class="menu-wrapper container-fluid">
         <div class="row">
-        <div class="logo-wrapper col-sm-3">
-            <a href="/">
-                <Logo />
+        <div class="logo-wrapper col-lg-3 col-12">
+            <a href="https://theadultchair.com/">
+                <img src={Logo} alt="" />
             </a>
         </div>
-        <div class="nav col-sm-9">
-            <div class="col">
+        <div class="nav col-lg-9 col-12">
+            <div class="row">
+            <div class="col-12 col-md-8">
                 <ul>
-                <li><a href="/#">The Adult Chair</a></li>
-                <li><a href="/#">Courses</a></li>
-                <li><a href="/#">2023 Journaling Challenging</a></li>
-                <li><a href="/#">Find A TAC Coach</a></li>
-                <li><a href="/#">Courses</a></li>
-                <li><a href="/#">Contact</a></li>
-                <li class="search" onClick={() => setToggle(!toggle)}><img alt="" src="https://michellechalfant.com/careers/icons/search-icon.png" /></li>
-                {!toggle && (
-                <div class="form-overlay">
-                    <button class="exit" onClick={() => setToggle(!toggle)}>
-                        X
-                    </button>
-                    <form action="/" method="get">
-                        <label>Search: </label>
-                <input type="text" name="s"></input>
-                <button type="submit"><img alt="" src="https://michellechalfant.com/careers/icons/search-icon.png" /></button>
-            </form>
-            </div> )}
-            </ul>
+                <li><a href="https://theadultchair.com/" target="_blank" rel="noreferrer" >Home</a></li>
+                <li><a href="https://theadultchair.com/podcasts" target="_blank" rel="noreferrer" >Podcast</a></li>
+                <li><a href="https://michellechalfant.com/careers/">Careers</a></li>
+                <li><a href="https://theadultchair.com/contact-us" target="_blank" rel="noreferrer" >Contact</a></li>
+            </ul> 
+            </div>
             </div>
             
         </div>
